c152790b04eb8c2f85910b6595b4baa8a7ff9e3a
tajongnoel <payzynoel@gmail.com>
2024-06-01 06:18:01 +0800

    Add files via upload

---
+++ Motors/DC motor/DCMotor.ino
@@ -0,0 +1,18 @@
+int m1=13, m2=12, en=9;
+int pot=A0, st=0;
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(m1, OUTPUT);
+  pinMode(m2, OUTPUT);
+  pinMode(en, OUTPUT);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  st=analogRead(pot)/4;
+  analogWrite(en, st);
+  digitalWrite(m1, HIGH);
+  digitalWrite(m2, LOW);
+
+}
---
+++ Motors/Stepper motor/StepperMotorInterface.ino
@@ -0,0 +1,20 @@
+#include<Stepper.h>
+const int spr=4;
+Stepper steper (spr, 8,9,10,11);
+
+void setup() {
+  
+ // put your setup code here, to run once:
+ steper.setSpeed (10);
+ Serial.begin (9600);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  steper.step (40);
+  delay (600);
+  steper.step (-40);
+  delay (600);
+
+}
